067cd9bae1dfc07949fe3a7b73535ccaf6096db3
Ludivinenadeau <ludivine.nadeau@gmail.com>
2021-12-14 14:05:43 +0100

    add header and footer couche web

--- web/app/Views/templates/footer.php
+++ web/app/Views/templates/footer.php
@@ -1,6 +1,17 @@
-    <em>&copy; 2021</em>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<div class="container-fluid" id="footer">
+    <div class="row">
+        <div class="col">Nous contacter</div>
+        <div class="col">Politique de protection des données personnelles</div>
+        <div class="col-4">Newsletter?</div>
+    </div>
+    <div class="row">
+        <div class="col">Plan du site</div>
+        <div class="col">Politique de confidentialité</div>
+        <div class="col-4"><input placeholder="Votre adresse mail:"></input></div>
+    </div>
+    <div class="row">
+        <div class="col">Nous trouver</div>
+        <div class="col">Politique des cookies</div>
+        <div class="col-4"><button type="button" class="btn btn-dark">ok</button></div>
+    </div>
+</div>
--- web/app/Views/templates/header.php
+++ web/app/Views/templates/header.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
 
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css" />
-    <title><?= esc($title??'Bienvenue') ?></title>
+    <title>Immo'Magic!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
+    <script src="sweetalert2.all.min.js" defer></script>
+    <script src="https://kit.fontawesome.com/161626f7e5.js" defer></script>
+
+
 </head>
-
 <body>
+<h1><?= esc($title) ?></h1> <!-- esc() fction globale fournie par CI pour aider à prevenir des attaques CSS -->
+<div class="container" id="header">
+    <div id="title"><h1 id="rotate"><b>Immo'Magic</b></h1><em id="slogan"> Le bien de vos rêves est là!</em></div>
+        <div class="icons">
+            <i class="fas fa-heart fa-2x"></i>
+            <i class="fas fa-user-circle fa-2x"></i>
+            <i class="fab fa-facebook-square fa-2x"></i>
+            <i class="fab fa-twitter fa-2x"></i>
+            <i class="fas fa-search-location fa-2x"></i>
+        </div>
+</div>
 
-    <h1 class="text-center mb-4"><?= esc($title??'Bienvenue') ?></h1>
