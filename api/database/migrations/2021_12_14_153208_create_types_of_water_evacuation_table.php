5d82b2c458453e17b304edc1f218a37ebcc76c5e
MaximePag <max_p@live.fr>
2021-12-17 10:20:30 +0100

    fix migration types

--- api/database/migrations/2021_12_14_153208_create_types_of_water_evacuation_table.php
+++ api/database/migrations/2021_12_14_153208_create_types_of_water_evacuation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypeOfWaterEvacuationTable extends Migration
+class CreateTypesOfWaterEvacuationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTypeOfWaterEvacuationTable extends Migration
      */
     public function up()
     {
-        Schema::create('typeOfWaterEvacuation', function (Blueprint $table) {
+        Schema::create('typesOfWaterEvacuation', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
         });
@@ -26,6 +26,6 @@ class CreateTypeOfWaterEvacuationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('typeOfWaterEvacuation');
+        Schema::dropIfExists('typesOfWaterEvacuation');
     }
 }
