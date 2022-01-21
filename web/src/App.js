6beddbb32291344cc3cecd2f49e904cb2739fb4b
BakariF <bakkfofhit@live.fr>
2022-01-21 15:13:59 +0100

    pas ouf

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,10 @@
-import './assets/css/style.css';
-import './components/Navbar';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './views/Home';
-import Contact from './views/Contact';
-import Http404 from './views/Http404';
+import "./assets/css/style.css";
+import "./components/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Contact from "./views/Contact";
+import Register from "./views/Register";
+import Http404 from "./views/Http404";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/contact" exact component={Contact} />
+        <Route path="/register" exact component={Register} />
         <Route component={Http404} />
       </Switch>
     </BrowserRouter>
---
+++ web/src/views/Register.js
@@ -0,0 +1,51 @@
+import Navbar from "../components/Navbar";
+import React from "react";
+class Register extends React.Component {
+  //Le moyen le plus simple de définir un composant consiste à écrire une fonction JavaScript
+  /*Exemple: class Welcome extends React.Component {
+  render() {
+    return <h1>Bonjour, {this.props.name}</h1>;
+  }
+} */
+  //Cette fonction est un composant React valide car elle accepte un seul argument « props » (qui signifie « propriétés ») contenant des données, et renvoie un élément React
+  constructor(props) {
+    super(props);
+    this.state = { name: "", password: "" };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  //s'occupe de mettre à jour les champs quand on les modifie
+  handleInputChange(event) {
+    this.setState({ value: event.target.name });
+    this.setState({ value: event.target.password });
+  }
+  //envoi la réponse du formulaire
+  handleSubmit(event) {
+    alert("mon retour :" + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Nom"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="mot de passe"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" Value="Envoyer" />
+        </form>
+      </div>
+    );
+  }
+}
+export default Register;
