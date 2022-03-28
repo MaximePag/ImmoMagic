454bf51eca5e28f8bd541ad6605ad71d805bc2f7
MaximePag <max_p@live.fr>
2022-03-28 15:07:56 +0200

    toto

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -96,7 +96,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AuthServiceProvider::class);
 //$app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
-$app->register(Chuckrincon\LumenConfigDiscover\DiscoverServiceProvider::class);
+// $app->register(Chuckrincon\LumenConfigDiscover\DiscoverServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
